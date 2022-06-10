6757fd09a39b0b7cabeb96e507e6d2897c4068cf
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-10 14:44:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName="bob";
+var bestCustomer;
+const leastFavouriteCustomer="somebody";
+
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer="not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+}
+
+function changeLeastFavouriteCustomer(){
+    leastFavouriteCustomer="somebody1";
+}
